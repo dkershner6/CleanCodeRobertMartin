ce864de67088b8d74af66bfd86bf1535bf78a302
Derek Kershner <kershner@eformedpartners.com>
2019-08-31 12:52:19 -0700

    Just starting out

---
+++ 14/14-1.java
@@ -0,0 +1,13 @@
+public class Args {
+  public static void main(String[] args) {
+    try {
+      Args arg = new Args("l,p#,d*", args);
+      boolean logging = arg.GetBoolean('l');
+      int port = arg.getInt('p');
+      String directory = arg.getString('d');
+      executeApplication(logging, port, directory);
+    } catch (ArgsException e) {
+      System.out.printf("Argument error: %s\n", e.errorMessage);
+    }
+  }
+}
---
+++ 14/14-2.java
@@ -0,0 +1,8 @@
+package com.objectmentor.utilities.args;
+
+import staticcom.objectmentor.utilities.args.ArgsException.ErrorCode.*;
+import java.util.*;
+
+public class Args {
+  private Map<Character, ArgumentMarshaler> marshalers;
+}
